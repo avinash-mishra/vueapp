68991c252cdc98c2db395f686dea8681ca4d8fc4
avi <mishra@psinc.jp>
2017-09-12 18:52:35 +0900

    initial commit

---
+++ src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="test">
+      <h1>{{title}}</h1>
+      <h2>{{message}}</h2>
+      <p>{{user.firstName}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "test",
+  data: () => ({
+      title: "Hello world",
+      message: "good one",
+      user: {
+        firstName: "John",
+        lastName: "Doe"
+      }
+  })
+}
+</script>
+<style scoped>
+</style>
